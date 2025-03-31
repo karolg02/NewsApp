71675739235c4fb38092d4ed5511e79af06cf770
karolg02 <karolglanowski02@gmail.com>
2025-03-31 18:22:01 +0200

    +connecting with API

--- src/features/mainpage/MainPage.tsx
+++ src/features/mainpage/MainPage.tsx
@@ -1,10 +1,35 @@
 import {getNews} from "./api/getNews.ts";
-import {Button} from "@mantine/core";
+import {useEffect, useState} from "react";
+import {Article} from "../../types/dataType.ts";
+import {Button, Image, List, Paper, Text} from "@mantine/core";
 
 export const MainPage= () => {
+    const [articles, setArticles] = useState<Article[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getNews();
+            if (result) setArticles(result.articles);
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <>
-            <Button onClick={getNews}>Kliknij mnie</Button>
-        </>
-    )
+        <div>
+            <Paper shadow="xl" p="lg" radius="lg" style={{ maxWidth: '70%', margin: ' auto' }}>
+                <List p="lg" ml="xl" mr="xl">
+                    {articles.map((article) => (
+                        <div key={article.url}>
+                            <Text size="xl" fw={900}>{article.title}</Text>
+                            <Image src={article.urlToImage} alt={article.title} style={{width: "300px"}}/>
+                            <Text>{article.description}</Text>
+                            <Button>Przejdz do strony</Button>
+                            <hr/>
+                        </div>
+                    ))}
+                </List>
+            </Paper>
+        </div>
+    );
 }
--- src/features/mainpage/api/getNews.ts
+++ src/features/mainpage/api/getNews.ts
@@ -1,6 +1,20 @@
 import {API_KEY} from "../../../types/config.ts";
-import ky from "ky";
+import {NewsResponse} from "../../../types/dataType.ts";
 
-export const getNews = () => {
-    return ky.get(`https://newsapi.org/v2/everything?q=tesla&from=2025-02-28&sortBy=publishedAt&apiKey=${API_KEY}`).json();
+export const getNews = async () => {
+    try {
+        const response = await fetch(
+            `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${API_KEY}`
+        );
+
+        if (!response.ok) {
+            throw new Error("Błąd API");
+        }
+
+        const data: NewsResponse = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Błąd przy pobieraniu newsów:", error);
+        return null;
+    }
 }
